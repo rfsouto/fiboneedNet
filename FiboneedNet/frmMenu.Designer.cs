fff9011ad1f7832268264d7917bbb99c9192530c
Rubén Fernández Souto <rfernandezsouto@gmail.com>
2013-08-30 17:09:12 +0200

    first commit

---
+++ FiboneedNet/frmMenu.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace FiboneedNet
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.tbNumber = new System.Windows.Forms.MaskedTextBox();
+            this.bntCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(12, 18);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(211, 18);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "Número base para calcular";
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(229, 19);
+            this.tbNumber.Mask = "9999999999";
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(186, 20);
+            this.tbNumber.TabIndex = 1;
+            // 
+            // bntCalcular
+            // 
+            this.bntCalcular.Location = new System.Drawing.Point(314, 45);
+            this.bntCalcular.Name = "bntCalcular";
+            this.bntCalcular.Size = new System.Drawing.Size(101, 22);
+            this.bntCalcular.TabIndex = 2;
+            this.bntCalcular.Text = "Calcular";
+            this.bntCalcular.UseVisualStyleBackColor = true;
+            this.bntCalcular.Click += new System.EventHandler(this.bntCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Location = new System.Drawing.Point(12, 70);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(403, 117);
+            this.lblResultado.TabIndex = 0;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 197);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.bntCalcular);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "frmMenu";
+            this.Text = "FiboneedNet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.MaskedTextBox tbNumber;
+        private System.Windows.Forms.Button bntCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ FiboneedNet/frmMenu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiboneedNet
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCalcular_Click(object sender, EventArgs e)
+        {
+            long n = long.Parse(this.tbNumber.Text);//Se lee el numero a calcular 
+
+            //Vaciamos la etiqueta de resultado.
+            lblResultado.Text = "";
+            DateTime tiempo1 = DateTime.Now;//Inicio medida tiempo
+            string txtres = "Fibonacci Iterativo para n = " + n + ": " + Fibonacci(n);
+            DateTime tiempo2 = DateTime.Now;//Fin medida tiempo
+            txtres += "\nTiempo del Fibonacci recursivo: " + new TimeSpan(tiempo2.Ticks - tiempo1.Ticks).ToString() + "\n";
+            tiempo1 = DateTime.Now;//Inicio medida tiempo
+            txtres += "\nFibonacci Recursivo para n = " + n + ": " + FiboRecur(n);
+            tiempo2 = DateTime.Now;//Fin medida tiempo
+            txtres += "\nTiempo del Fibonacci recursivo: " + new TimeSpan(tiempo2.Ticks - tiempo1.Ticks).ToString() + "\n";
+            lblResultado.Text = txtres;
+            lblResultado.Refresh();
+        }
+
+        public long Fibonacci(long n)
+        {
+            //a representa el valor n-1
+            //b representa el valor n
+            long a = 1, b = 1;
+            for (long i = 2; i <= n; i++)
+            {
+                long c = a + b;
+                a = b;
+                b = c;
+            }
+            return a;
+        }
+
+        public long FiboRecur(long n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return FiboRecur(n - 1) + FiboRecur(n - 2);
+            }
+        }
+    }
+}
